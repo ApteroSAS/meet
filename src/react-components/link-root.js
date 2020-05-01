0327386b3b8673de082f240621d3ee37fc41eb69
Pierre-Henri <pierreh37@gmail.com>
2020-05-01 13:18:00 +0200

    bug fix on link page on oculus

--- src/react-components/link-root.js
+++ src/react-components/link-root.js
@@ -282,11 +282,11 @@ class LinkRoot extends Component {
                 </div>
               )}
             </div>
-            <div className={styles.createLink}>
+            {/*<div className={styles.createLink}>
               <a href="/">
                 <FormattedMessage id="link.create_a_room" />
               </a>
-            </div>
+            </div>*/}
           </div>
         </div>
       </IntlProvider>
