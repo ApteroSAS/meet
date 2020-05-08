48be76b59e53dab070765ca0a2f8b30d04741355
Pierre-Henri <pierreh37@gmail.com>
2020-05-08 16:01:01 +0200

    2.1.8

--- src/components/media-loader.js
+++ src/components/media-loader.js
@@ -458,8 +458,8 @@ AFRAME.registerComponent("media-loader", {
             linkedMediaElementAudioSource
           })
         );
-        if (this.el.components["position-at-box-shape-border__freeze"]) {
-          this.el.setAttribute("position-at-box-shape-border__freeze", { dirs: ["forward", "back"] });
+        if (this.el.components["position-at-border__freeze"]) {
+          this.el.setAttribute("position-at-border__freeze", { isFlat: true });
         }
         if (this.el.components["position-at-border__freeze-unprivileged"]) {
           this.el.setAttribute("position-at-border__freeze-unprivileged", { isFlat: true });
@@ -477,7 +477,7 @@ AFRAME.registerComponent("media-loader", {
             if (contentSubtype === "photo-camera") {
               this.el.setAttribute("hover-menu__photo", {
                 template: "#photo-hover-menu",
-                dirs: ["forward", "back"]
+                isFlat: true
               });
             }
           },
@@ -498,8 +498,8 @@ AFRAME.registerComponent("media-loader", {
           })
         );
 
-        if (this.el.components["position-at-box-shape-border__freeze"]) {
-          this.el.setAttribute("position-at-box-shape-border__freeze", { dirs: ["forward", "back"] });
+        if (this.el.components["position-at-border__freeze"]) {
+          this.el.setAttribute("position-at-border__freeze", { isFlat: true });
         }
         if (this.el.components["position-at-border__freeze-unprivileged"]) {
           this.el.setAttribute("position-at-border__freeze-unprivileged", { isFlat: true });
@@ -527,8 +527,8 @@ AFRAME.registerComponent("media-loader", {
           { once: true }
         );
 
-        if (this.el.components["position-at-box-shape-border__freeze"]) {
-          this.el.setAttribute("position-at-box-shape-border__freeze", { dirs: ["forward", "back"] });
+        if (this.el.components["position-at-border__freeze"]) {
+          this.el.setAttribute("position-at-border__freeze", { isFlat: true });
         }
         if (this.el.components["position-at-border__freeze-unprivileged"]) {
           this.el.setAttribute("position-at-border__freeze-unprivileged", { isFlat: true });
@@ -578,15 +578,15 @@ AFRAME.registerComponent("media-loader", {
             if (await isLocalHubsAvatarUrl(src)) {
               this.el.setAttribute("hover-menu__hubs-item", {
                 template: "#avatar-link-hover-menu",
-                dirs: ["forward", "back"]
+                isFlat: true
               });
             } else if ((await isHubsRoomUrl(src)) || ((await isLocalHubsSceneUrl(src)) && mayChangeScene)) {
               this.el.setAttribute("hover-menu__hubs-item", {
                 template: "#hubs-destination-hover-menu",
-                dirs: ["forward", "back"]
+                isFlat: true
               });
             } else {
-              this.el.setAttribute("hover-menu__link", { template: "#link-hover-menu", dirs: ["forward", "back"] });
+              this.el.setAttribute("hover-menu__link", { template: "#link-hover-menu", isFlat: true });
             }
             this.onMediaLoaded(SHAPE.BOX);
           },
@@ -606,8 +606,8 @@ AFRAME.registerComponent("media-loader", {
             batch
           })
         );
-        if (this.el.components["position-at-box-shape-border__freeze"]) {
-          this.el.setAttribute("position-at-box-shape-border__freeze", { dirs: ["forward", "back"] });
+        if (this.el.components["position-at-border__freeze"]) {
+          this.el.setAttribute("position-at-border__freeze", { isFlat: true });
         }
         if (this.el.components["position-at-border__freeze-unprivileged"]) {
           this.el.setAttribute("position-at-border__freeze-unprivileged", { isFlat: true });
@@ -640,7 +640,7 @@ AFRAME.registerComponent("media-pager", {
     this.onSnap = this.onSnap.bind(this);
     this.update = this.update.bind(this);
 
-    this.el.setAttribute("hover-menu__pager", { template: "#pager-hover-menu", dirs: ["forward", "back"] });
+    this.el.setAttribute("hover-menu__pager", { template: "#pager-hover-menu", isFlat: true });
     this.el.components["hover-menu__pager"].getHoverMenu().then(menu => {
       // If we got removed while waiting, do nothing.
       if (!this.el.parentNode) return;
--- src/components/position-at-border.js
+++ src/components/position-at-border.js
@@ -193,7 +193,7 @@ AFRAME.registerComponent("position-at-border", {
         // Put in front of the flat media, on the side where the user is
         desiredCenterPoint.copy(this.meshCenter).add(
           centerToBorder
-            .set(0, 0, this.meshHalfExtents.z + this.targetHalfExtents.z + 1)
+            .set(0, 0, this.meshHalfExtents.z + this.targetHalfExtents.z + 0.02)
             .multiplyScalar(needsYRotate ? -1 : 1)
             .applyMatrix4(currentMeshRotation)
         );
