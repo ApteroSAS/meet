f45699935bb83953afb9eb02be2171837808f827
Pierre-Henri <pierreh37@gmail.com>
2020-10-28 19:36:18 +0100

    bug fix sharescreen >50

--- src/aptero/service/StaticObjectManipulator.js
+++ src/aptero/service/StaticObjectManipulator.js
@@ -21,12 +21,13 @@ const floatEqual = (a, b, ep) => {
 export class StaticObjectManipulator {
 
   deactivatedEntity = {};
+  LastDeactivatedEntitySend = null;
   staticEntities = {};
 
   constructor() {
   }
 
-  start(){
+  start() {
 
     networkService.eventEmitter.once("adapter_ready", () => {
       //When the network is ready on our side we store the static media
@@ -40,33 +41,52 @@ export class StaticObjectManipulator {
     //when a new client connects we send him the state of the room what is deactivated etc...
     //////////////////////////
     networkService.onMessage("StaticObjectManipulator-update", (data) => {
-      if(data && data.deactivatedEntities && NAF.clientId!==data.clientID) {
-        console.log("recv update");
-        this.deactivatedEntity = data.deactivatedEntities;
-        this.updateVisibilityLocal();
+      if (data && data.deactivatedEntities && NAF.clientId !== data.fromClientID && (data.toClientID === "ANY" ? true : NAF.clientId === data.toClientID)) {
+        if(!this.mapEquals(this.deactivatedEntity,data.deactivatedEntities)) {
+          this.deactivatedEntity = data.deactivatedEntities;
+          if(data.toClientID === "ANY") {
+            //everyone already uptodate
+            this.LastDeactivatedEntitySend = { ...this.deactivatedEntity };
+          }
+          console.log("recv update from " + data.fromClientID + " to " + data.toClientID, this.deactivatedEntity);
+          this.updateVisibilityLocal();
+        }
       }
     });
 
     waitForDOMContentLoaded().then(() => {
-        document.body.addEventListener("clientConnected", (evt) => {
-          console.log("clientConnected",evt);
-          if(Object.keys(this.deactivatedEntity).length!==0) {
-            this.privateSendUpdateViaNetwork();
-          }
-        });
+      document.body.addEventListener("clientConnected", (evt) => {
+        let clientID = evt.detail.clientId;
+        console.log("clientConnected", evt);
+        if (Object.keys(this.deactivatedEntity).length !== 0) {
+            this.privateSendUpdateViaNetwork(clientID,true);
+        }
+      });
     });
     ///////////////////////////
     //end
     ///////////////////////////
   }
 
-  privateSendUpdateViaNetwork(){
-    networkService.sendMessage("StaticObjectManipulator-update",{deactivatedEntities:this.deactivatedEntity,clientID:NAF.clientId});
-    console.log("send update");
-    this.updateVisibilityLocal();
+  mapEquals(mapA,MapB){
+    return JSON.stringify(mapA)===JSON.stringify(MapB);
   }
 
-  updateVisibilityLocal(){
+  privateSendUpdateViaNetwork(clientID,force=false) {
+    if (force || this.LastDeactivatedEntitySend == null || !this.mapEquals(this.LastDeactivatedEntitySend,this.deactivatedEntity)) {
+      //do not send update if nothing to update
+      networkService.sendMessage("StaticObjectManipulator-update", {
+        deactivatedEntities: this.deactivatedEntity,
+        fromClientID: NAF.clientId,
+        toClientID: clientID
+      });
+      this.LastDeactivatedEntitySend = { ...this.deactivatedEntity };
+      console.log("send update from "+NAF.clientId+" to "+clientID,this.deactivatedEntity);
+      this.updateVisibilityLocal();
+    }
+  }
+
+  updateVisibilityLocal() {
     const deactivatedEntities = Object.keys(this.deactivatedEntity);
     this.makeAllVisible();
     deactivatedEntities.forEach(key => {
@@ -74,54 +94,53 @@ export class StaticObjectManipulator {
     });
   }
 
-  markInvisible(netID){
+  markInvisible(netID) {
     const ent = NAF.entities.entities[netID];
     if (!ent) return;
+    this.deactivatedEntity[netID] = netID;
     ent.object3D.visible = false;
-    console.info("deactivate : "+netID);
+    console.info("deactivate : " + netID);
   }
 
-  deactivateNode(netID,fromNetwork) {
-    if(this.isStaticMedia(netID)) {
-      if(this.deactivatedEntity[netID]){
+  deactivateNode(netID, notifyNetwork=true) {
+    if (this.isStaticMedia(netID)) {
+      if (this.deactivatedEntity[netID]) {
         return;
       }
-      this.deactivatedEntity[netID] = netID;
       this.markInvisible(netID);
-      if(!fromNetwork) {
-        this.privateSendUpdateViaNetwork();
+      if (notifyNetwork) {
+        this.privateSendUpdateViaNetwork("ANY");
       }
-    }else{
+    } else {
       throw new Error("deactivating interactable media");
     }
   }
 
   reactivateNodeAt(position) {
     Object.keys(this.staticEntities).forEach(staticKey => {
-        const ax = this.staticEntities[staticKey].position.x;
-        const ay = this.staticEntities[staticKey].position.y;
-        const az = this.staticEntities[staticKey].position.z;
-        if (floatEqual(ax, position.x, 0.001) && floatEqual(ay, position.y, 0.001) && floatEqual(az, position.z, 0.001)) {
-          this.reactivateNode(staticKey);
-        }
+      const ax = this.staticEntities[staticKey].position.x;
+      const ay = this.staticEntities[staticKey].position.y;
+      const az = this.staticEntities[staticKey].position.z;
+      if (floatEqual(ax, position.x, 0.001) && floatEqual(ay, position.y, 0.001) && floatEqual(az, position.z, 0.001)) {
+        this.reactivateNode(staticKey);
+      }
     });
   }
 
 
-  reactivateNode(staticKey,fromNetwork){
-    console.info("activate : "+staticKey);
+  reactivateNode(staticKey) {
+    console.info("activate : " + staticKey);
     delete this.deactivatedEntity[staticKey];
     const ent = NAF.entities.entities[staticKey];
-    ent.object3D.visible=true;
-    if(!fromNetwork) {
-      this.privateSendUpdateViaNetwork();
-    }
+    ent.object3D.visible = true;
+    this.privateSendUpdateViaNetwork("ANY");
   }
 
   cleanUp() {
     if (!this.cleanUpState) {
       this.cleanUpState = true;
       setTimeout(() => {
+        this.LastDeactivatedEntitySend = null;//after a cleanup
         this.reactivateAllAndRemoveDuplicate();
         this.cleanUpState = false;
       }, 1000);
@@ -135,7 +154,6 @@ export class StaticObjectManipulator {
 
   reactivateAllAndRemoveDuplicate() {
     const entKeys = Object.keys(NAF.entities.entities);
-    this.deactivatedEntity={};//mark all as activated
     this.makeAllVisible();
     entKeys.forEach(keyA => {
       const entA = NAF.entities.entities[keyA];
@@ -149,16 +167,18 @@ export class StaticObjectManipulator {
         const bz = entB.object3D.position.z;
         if (floatEqual(ax, bx, 0.001) && floatEqual(ay, by, 0.001) && floatEqual(az, bz, 0.001) && keyA !== keyB) {
           if (this.isStaticMedia(keyA)) {
-            this.deactivateNode(keyA);
+            this.deactivateNode(keyA,false);
           } else if (this.isStaticMedia(keyB)) {
-            this.deactivateNode(keyB);
+            this.deactivateNode(keyB,false);
           }
         }
       });
     });
   }
 
-  makeAllVisible(){
+  makeAllVisible() {
+    console.log("makeAllVisible");
+    this.deactivatedEntity = {};//mark all as activated
     Object.keys(this.staticEntities).forEach(staticKey => {
       const ent = NAF.entities.entities[staticKey];
       ent.object3D.visible = true;
@@ -169,15 +189,15 @@ export class StaticObjectManipulator {
     if (this.isStaticMedia(key)) {
       this.staticEntities[key] = {
         entity: entity,
-        position:{
+        position: {
           x: entity.object3D.position.x,
           y: entity.object3D.position.y,
-          z: entity.object3D.position.z,
+          z: entity.object3D.position.z
         },
-        scale:{
-          x:entity.object3D.scale.x,
+        scale: {
+          x: entity.object3D.scale.x,
           y: entity.object3D.scale.y,
-          z: entity.object3D.scale.z,
+          z: entity.object3D.scale.z
         },
         src: entity.components["media-loader"].data.src
       };
