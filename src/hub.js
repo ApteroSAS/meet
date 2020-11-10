f8cea0bc0c8465392404e0a212e5e3bd0a33af37
Pierre-Henri <pierreh37@gmail.com>
2020-11-10 14:32:31 +0100

    twilio integration (Harsh release)

--- src/hub.js
+++ src/hub.js
@@ -376,6 +376,7 @@ function setupPeerConnectionConfig(adapter, host, turn) {
     ];
   }
 
+  peerConnectionConfig.iceServers = window.iceServers || peerConnectionConfig.iceServers; //APTERO
   adapter.setPeerConnectionConfig(peerConnectionConfig);
 }
 
@@ -794,7 +795,27 @@ function checkForAccountRequired() {
   )}`;
 }
 
+async function fetchICEservers() {
+  const myHeaders = new Headers();
+  myHeaders.append(
+    "Authorization",
+    "Basic U0tjYzMxZDM0N2YxOGVlYmM3MjNiMDgzNTc0ZTE2MWJjMDp6bFM1TjNmbGhWMEdOc0ZLcG1vbFBucW1TcWdMbEliNQ=="
+  );
+
+  const requestOptions = {
+    method: "POST",
+    headers: myHeaders,
+    redirect: "follow"
+  };
+  const twilioApi = "https://api.twilio.com/2010-04-01/Accounts/AC2b4884e41acc8b8ed9f7ee795969d76b/Tokens.json";
+  const resp = await fetch(twilioApi, requestOptions);
+  const servers = await resp.json();
+  window.iceServers = [...servers.ice_servers];
+  return servers;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
+  await fetchICEservers();
   await store.initProfile();
 
   const canvas = document.querySelector(".a-canvas");
